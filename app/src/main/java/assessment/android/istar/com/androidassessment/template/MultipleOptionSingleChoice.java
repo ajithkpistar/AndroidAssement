f0043d080abceb6adcb0d5629f7e448462a0630b
Ajith <ajith@istarindia.com>
2016-12-20 21:05:17 +0530

    long lcik fixes

--- app/src/main/java/assessment/android/istar/com/androidassessment/template/MultipleOptionSingleChoice.java
+++ app/src/main/java/assessment/android/istar/com/androidassessment/template/MultipleOptionSingleChoice.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.widget.AppCompatRadioButton;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
